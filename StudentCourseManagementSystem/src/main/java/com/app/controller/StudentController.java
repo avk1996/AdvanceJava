eaa3dcb694f6a30e559521a7181b2bf01c1fe02e
abhishekverma25 <ababhi.verma@gmail.com>
2024-01-18 14:49:30 +0530

    corrected code

--- StudentCourseManagementSystem/src/main/java/com/app/controller/StudentController.java
+++ StudentCourseManagementSystem/src/main/java/com/app/controller/StudentController.java
@@ -5,13 +5,13 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.app.models.Student;
 import com.app.service.StudentService;
 
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -31,5 +31,7 @@ public class StudentController {
 	public Student insertStudent(@RequestBody Student student) {
 		return studentService.insertStudent(student);
 	}
+	
+	
 
 }
